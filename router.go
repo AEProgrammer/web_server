f4e61bf20de77e4ce80a35d254ddf504be6a4ee1
nicolehliu <nicolehliu@tencent.com>
2023-07-26 00:08:23 +0800

    create route tree and fix unit test bug

--- router.go
+++ router.go
@@ -1,5 +1,7 @@
 package web_server
 
+import "strings"
+
 type router struct {
 	// 一个http方法对应一棵树
 	// http method -> 路由数根结点
@@ -7,11 +9,45 @@ type router struct {
 }
 
 func newRouter() *router {
-	return &router{}
+	return &router{
+		trees: map[string]*node{},
+	}
 }
 
 func (r *router) AddRoute(method string, path string, handleFunc HandleFunc) {
+	root, ok := r.trees[method]
+	if !ok {
+		// 没有根结点
+		root = &node{
+			path: "/",
+		}
+		r.trees[method] = root
+	}
 
+	// 切割path
+	path = path[1:]
+	segs := strings.Split(path, "/")
+	for _, seg := range segs {
+		// 递归找准位置 中间节点如果不存在要新建节点
+		child := root.childOrCreate(seg)
+		root = child
+	}
+	root.handler = handleFunc
+}
+
+func (n *node) childOrCreate(seg string) *node {
+	if n.children == nil {
+		n.children = make(map[string]*node)
+	}
+
+	child, ok := n.children[seg]
+	if !ok {
+		child = &node{
+			path: seg,
+		}
+		n.children[seg] = child
+	}
+	return child
 }
 
 type node struct {
@@ -23,3 +59,5 @@ type node struct {
 	// 用户注册的业务逻辑
 	handler HandleFunc
 }
+
+
